85c62bd524fa388452aa3cdc3d09288c58beb05b
mitish13 <mitish1308@gmail.com>
2021-01-21 15:13:22 +0530

    Third commit

--- admin/scripts/user.js
+++ admin/scripts/user.js
@@ -3,16 +3,17 @@ const userEmail=document.getElementById('userEmail');
 const userPassword=document.getElementById('userPassword');
 const bday=document.getElementById('bday');
 const form=document.getElementById('form');
-const div=document.getElementById('list');
+const table=document.getElementById('table');
 
 var data=[];
 
 
 form.addEventListener('submit',adduser);    
 
-// window.onload=()=>{
-//     localStorage.removeItem('users')
-// }
+window.onload=()=>{
+    const usersArray=JSON.parse(localStorage.getItem('users'));
+    writeOnPage(usersArray);
+}
 
 function adduser(e){
     e.preventDefault();
@@ -31,11 +32,32 @@ function adduser(e){
 }
 
 function NewEntry(){
+    var dob=new Date(bday.value);
 
+     //calculate month difference from current date in time  
+     var month_diff = Date.now() - dob.getTime();  
+      
+     //convert the calculated difference in date format  
+     var age_dt = new Date(month_diff);   
+       
+     //extract year from date      
+     var year = age_dt.getUTCFullYear();  
+       
+     //now calculate the age of the user  
+     var age = Math.abs(year - 1970);  
+  
+     //Action prop
+    var action1="Edit";
+    var action2="delete";
+    
     this.userName=userName.value;
     this.userEmail=userEmail.value;
     this.userPassword=userPassword.value;
     this.bday=bday.value;
+    this.age=age;
+    this.edit=action1;
+    this.del=action2;
+
 
 }
 
@@ -45,17 +67,43 @@ function viewUser(){
     const email=[];
     const password=[];
     const userBday=[];
+    const userAge=[]
 
    usersArray.forEach((elem)=>{
        uname.push(elem.userName);
        email.push(elem.userEmail);
        password.push(elem.userPassword);
        userBday.push(elem.bday);
+        userAge.push(elem.age);
    })
 
-   writeOnPage();
+
 
 }
-function writeOnPage(){
+function writeOnPage(usersArray){
+    data=usersArray;    
+
+    for(var i=0;i<data.length;i++){
+        
+        //row 
+        var tr=document.createElement('tr');
+        for(const prop in data[i]){
+            var td=document.createElement('td');
+            td.innerText=data[i][prop];
+
+            // action
+
+            
+
+            tr.appendChild(td);
+
+        }   
+
+       
+        table.appendChild(tr);
     
+    }
+
+    
+
 }
